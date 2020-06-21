0d8a5bf019c71cbab9b233173760fb5f449d8d04
Josip Miskovic <josipmiskovic@gmail.com>
2020-06-21 22:07:08 +0300

    Requesting microphone

--- LovrApp/java/org/lovr/oculusmobile/MainActivity.java
+++ LovrApp/java/org/lovr/oculusmobile/MainActivity.java
@@ -1,12 +1,9 @@
 /************************************************************************************
-
 Filename    :   MainActivity.java
 Content     :   
 Created     :   
 Authors     :   
-
 Copyright   :   Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved.
-
 *************************************************************************************/
 package org.lovr.oculusmobile;
 
@@ -15,12 +12,10 @@ When using NativeActivity, we currently need to handle loading of dependent
 shared libraries manually before a shared library that depends on them
 is loaded, since there is not currently a way to specify a shared library
 dependency for NativeActivity via the manifest meta-data.
-
 The simplest method for doing so is to subclass NativeActivity with
 an empty activity that calls System.loadLibrary on the dependent
 libraries, which is unfortunate when the goal is to write a pure
 native C/C++ only Android activity.
-
 A native-code only solution is to load the dependent libraries
 dynamically using dlopen(). However, there are a few considerations,
 see: https://groups.google.com/forum/#!msg/android-ndk/l2E2qh17Q6I/wj6s_6HSjaYJ
@@ -31,11 +26,53 @@ a dlopen on the same lib a second time.
 infinitely looping when libraries have circular dependencies.
 */
 
-public class MainActivity extends android.app.NativeActivity {
+import android.Manifest;
+import android.app.NativeActivity;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import 	android.util.Log;
+
+public class MainActivity extends android.app.NativeActivity implements ActivityCompat.OnRequestPermissionsResultCallback
+{
 	
 	/** Load jni .so on initialization */
-	static {
-		System.loadLibrary( "vrapi" );
-		System.loadLibrary( "lovractivity" );
-	}
+    static {
+        System.loadLibrary( "vrapi" );
+        System.loadLibrary( "lovractivity" );
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("LOVR", "MainActivity.onCreate()");
+        RequestPermissionsIfNeeded();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            Log.i("LOVR", "RECORD_AUDIO permissions have now been granted.");
+        }
+        else
+        {
+            Log.i("LOVR", "RECORD_AUDIO permissions were denied.");
+        }
+    }
+    
+    private void RequestPermissionsIfNeeded()
+    {
+        int existingPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        if(existingPermission != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.i("LOVR", "Asking for RECORD_AUDIO permissions.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+        else
+        {
+          Log.i("LOVR", "RECORD_AUDIO permissions already granted.");
+        }
+    }
 }
